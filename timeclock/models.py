848483498834153086839290f2f25915d95225df
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2022-02-14 02:06:33 +0700

    update model and schema for clockin and clockout

--- timeclock/models.py
+++ timeclock/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.forms import DateTimeField
 
 # Create your models here.
 class ExtendUser(AbstractUser):
     email = models.EmailField(blank=False, max_length=255, verbose_name="email")
-
     USERNAME_FIELD = "username"
     EMAIL_FIELD = "email"
+
+class Clock(models.Model):
+    user = models.ForeignKey(ExtendUser, on_delete=models.DO_NOTHING)
+    clocked_in = models.DateTimeField()
+    clocked_out = models.DateTimeField()
+    
+class ClockedHours(models.Model):
+    today = models.IntegerField()
+    current_week = models.IntegerField()
+    current_month = models.IntegerField()
+    
+
+    
--- timeclock/schema.py
+++ timeclock/schema.py
@@ -1,15 +1,48 @@
 from atexit import register
 import graphene
+from graphene_django import DjangoObjectType
 from graphql_auth.schema import UserQuery, MeQuery
 from graphql_auth import mutations
+from .models import Clock, ClockedHours
+from datetime import datetime
+
+class ClockType(DjangoObjectType):
+    class Meta:
+        model = Clock
+        fields = ("id", "user", "clocked_in", "clocked_out")
+class ClockedHoursType(DjangoObjectType):
+    class Meta:
+        model = ClockedHours
+        fields = ("today", "current_week", "current_month")
+
+class ClockInMutation(graphene.Mutation):
+    clock = graphene.Field(ClockType)
+    
+    @classmethod
+    def mutate(cls, root, info):
+        utctime = datetime.utcnow()
+        clock = Clock(clocked_in=utctime)
+        clock.save()
+        return ClockInMutation(clock=clock)
+        
+class ClockOutMutation(graphene.Mutation):
+    clock = graphene.Field(ClockType)
+    
+    @classmethod
+    def mutate(cls, root, info):
+        pass
+    
+class ClocedHoursType(DjangoObjectType):
+    class Meta:
+        model = ClockedHours
+        fields = ("today", "current_week", "current_month")
+    
 
 class AuthMutation(graphene.ObjectType):
     create_user = mutations.Register.Field()
-    verify_account = mutations.VerifyAccount.Field()
     obtain_token = mutations.ObtainJSONWebToken.Field()
-    update_account = mutations.UpdateAccount.Field()
-    # clock_in = 
-    # clock_out = 
+    clock_in = ClockInMutation.Field()
+    clock_out = ClockOutMutation.Field()
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
     
