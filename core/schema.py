79cd36893d7f2d9ffdfd210f46e3230c096bc63b
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2022-02-25 02:52:18 +0700

    rename obtain_token

--- core/schema.py
+++ core/schema.py
@@ -8,7 +8,7 @@ class Query(users.schema.Query, timeclock.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, timeclock.schema.Mutation, graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    obtain_token = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     
