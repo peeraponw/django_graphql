4efa145215e4a3a4e495189689d0e965fba2f38c
Peerapon Wechsuwanmanee <peerapon.w@gmail.com>
2022-02-28 03:10:40 +0700

    add resolved_clocked_hours function

--- core/schema.py
+++ core/schema.py
@@ -2,13 +2,15 @@ import graphene
 import graphql_jwt
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.db.models import Sum
 
 from users.schema import UserType
 from users.schema import CreateUser
 
-from timeclock.models import Clock
+from timeclock.models import Clock, ClockedHours
 from timeclock.schema import ClockType, ClockedHoursType
 from timeclock.schema import ClockIn, ClockOut
+
 
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
@@ -45,7 +47,40 @@ class Query(graphene.ObjectType):
         
     
     def resolve_clocked_hours(self, info, **kwargs):
-        pass
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception("Not logged in")
+        now = timezone.now()
+        this_year, this_week, today = now.isocalendar() # this follows ISO 8601 calendar but not Gregorian calendar
+        # today part
+        today_clock = Clock.objects.filter(user=user).filter(clocked_in__date__gte=now.date()).all()
+        today_hours = 0
+        for clock in today_clock:
+            clockin = clock.clocked_in
+            clockout = clock.clocked_out if clock.clocked_out else now
+            hours = (clockout - clockin).seconds / 3600
+            today_hours += hours
+        # this week part
+        this_week_clock = Clock.objects.filter(user=user).filter(clocked_in__week__gte=this_week).all()
+        this_week_hours = 0
+        for clock in this_week_clock:
+            clockin = clock.clocked_in
+            clockout = clock.clocked_out if clock.clocked_out else now
+            hours = (clockout - clockin).seconds / 3600
+            this_week_hours += hours
+        # this month part
+        this_month_clock = Clock.objects.filter(user=user).filter(clocked_in__month__gte=now.month).all()
+        this_month_hours = 0
+        for clock in this_month_clock:
+            clockin = clock.clocked_in
+            clockout = clock.clocked_out if clock.clocked_out else now
+            hours = (clockout - clockin).seconds / 3600
+            this_month_hours += hours
+        c = ClockedHours.objects.create(today=today_hours, current_week=this_week_hours, current_month=this_month_hours)
+        c.save()
+        return c
+        
+        
 
 class Mutation(graphene.ObjectType):
     obtain_token = graphql_jwt.ObtainJSONWebToken.Field()
